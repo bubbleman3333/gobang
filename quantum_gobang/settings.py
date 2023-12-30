e0227eb83b3dc67c10f37e8d18f58cec0f5e0edb
bubbleman3333 <keizaburo.takashiba@gmail.com>
2023-12-30 14:22:12 +0900

    require

--- quantum_gobang/settings.py
+++ quantum_gobang/settings.py
@@ -120,6 +120,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = "/home/bubbleman3003/gobang/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
